f2d6160d7183e8887756cae894cb80d802ee22fc
Liltar <liltar94@gmail.com>
2021-03-01 12:59:23 +0200

    init commit

--- roller_shutters/case_1.py
+++ roller_shutters/case_1.py
@@ -1,10 +1,21 @@
 # ставим на сигнализ:
 # если светло и разрешен упр ролетами - роллеты закрываются - даём импульс 4 сек на закрытие
 # если темно - ничего не делаем
+# что управляет ролетами - то, что дает импульс на электромотор
+# что управляет сигнализацией - хранит её статус
 
 # получаем время захода солнца
 from astral import LocationInfo, sun
 import datetime
-location = LocationInfo('Kiev')
-sunset_time = sun.sunset(location.observer, date=datetime.date.today(), tzinfo=location.timezone)
-current_time = datetime.datetime.now()
+from pytz import timezone
+location = LocationInfo()
+sunset_time = sun.sunset(location.observer, date=datetime.datetime.now(), tzinfo=location.timezone)
+london = timezone('Europe/London')
+current_time = london.localize(datetime.datetime.now())
+dark = current_time >= sunset_time
+while True:
+    if dark:
+
+        break
+    else:
+        break
